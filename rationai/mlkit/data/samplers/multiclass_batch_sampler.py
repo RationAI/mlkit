8d958f231245be3be10b758626edfb40b4ae4b13
Jakub Pekár <492788@mail.muni.cz>
2024-10-30 11:38:24 +0100

    feat: batch sampler with given distribution

---
+++ rationai/mlkit/data/samplers/multiclass_batch_sampler.py
@@ -0,0 +1,73 @@
+from collections.abc import Iterable, Iterator
+from typing import Any
+
+import numpy as np
+import pandas as pd
+from numpy.typing import NDArray
+from torch.utils.data import BatchSampler, RandomSampler, Sampler
+
+
+class MulticlassBatchSampler(BatchSampler):
+    def __init__(
+        self,
+        samplers: list[Sampler[int] | Iterable[int]],
+        distribution: list[float],
+        iterations_per_epoch: int,
+        batch_size: int,
+    ) -> None:
+        self.samplers = samplers
+        self.distribution = distribution
+        self.iterations_per_epoch = iterations_per_epoch
+        self.batch_size = batch_size
+
+    def __iter__(self) -> Iterator[list[int]]:
+        samplers_iters = [iter(sampler) for sampler in self.samplers]
+
+        for _ in range(self.iterations_per_epoch):
+            batch = []
+            for sampler, count in zip(
+                samplers_iters,
+                self._random_sample_counts(self.distribution, self.batch_size),
+                strict=False,
+            ):
+                batch.extend([next(sampler) for _ in range(count)])
+            yield batch
+
+    def __len__(self) -> int:
+        return self.iterations_per_epoch
+
+    @staticmethod
+    def _random_sample_counts(
+        distribution: list[float] | NDArray[np.float64], size: int
+    ) -> list[int]:
+        real_counts = distribution * size
+        counts = np.floor(real_counts).astype(int)
+        missing_counts = real_counts - counts
+
+        # If there are missing samples, add randomly weighted samples
+        if missing_counts.sum():
+            indices_to_add = (
+                np.random.rand(len(distribution))
+                * missing_counts
+                / np.sum(missing_counts)
+                # numpy's argsort sorts in ascending order so we need to reverse it
+            ).argsort()[-(size - np.sum(counts)) :]
+            counts += np.isin(np.arange(len(distribution)), indices_to_add)
+        return list(counts)
+
+
+class PDMulticlassBatchSampler(MulticlassBatchSampler):
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        stratify_by: None,
+        distribution: list[float],
+        iterations_per_epoch: int,
+        batch_size: int,
+        **kwargs: dict[str, Any],
+    ):
+        samplers: list[Sampler[int]] = [
+            RandomSampler(list(x.index))
+            for _, x in data.groupby(by=stratify_by, **kwargs)
+        ]
+        super().__init__(samplers, distribution, batch_size, iterations_per_epoch)
