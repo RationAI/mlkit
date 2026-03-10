7ae830792550cb7dd027d8685badbba8af9013a0
Jakub Pekár <492788@mail.muni.cz>
2026-03-10 08:22:14 +0100

    feat: Hugging Face datasets

--- rationai/mlkit/data/datasets/openslide_tiles_dataset.py
+++ rationai/mlkit/data/datasets/openslide_tiles_dataset.py
@@ -1,10 +1,10 @@
 from pathlib import Path
+from typing import Any
 
 import numpy as np
-import pandas as pd
+from datasets import Dataset as HFDataset
 from numpy.typing import NDArray
-from openslide import OpenSlide
-from PIL import Image
+from ratiopath.openslide import OpenSlide
 from torch.utils.data import Dataset
 
 
@@ -12,18 +12,18 @@ class OpenSlideTilesDataset(Dataset[NDArray[np.uint8]]):
     """Dataset for reading tiles from a single slide image.
 
     This dataset reads tiles from an OpenSlide image. The tiles are specified by a
-    DataFrame with columns ["x", "y"]. The RGBA tiles are converted to RGB before
+    table with columns ["x", "y"]. The RGBA tiles are converted to RGB before
     being returned.
 
     Attributes:
         slide (Path): Path to the slide image.
         level (int | str): Level of the slide to read. If int, it is used as the level.
-            If str, it is used as the column name in the tiles DataFrame.
+            If str, it is used as the column name in the tiles Dataset.
         tile_extent_x (int | str): Width of the tile. If int, it is used as the width.
-            If str, it is used as the column name in the tiles DataFrame.
+            If str, it is used as the column name in the tiles Dataset.
         tile_extent_y (int | str): Height of the tile. If int, it is used as the height.
-            If str, it is used as the column name in the tiles DataFrame.
-        tiles (pd.DataFrame): DataFrame with columns ["x", "y"] specifying the tiles
+            If str, it is used as the column name in the tiles Dataset.
+        tiles (HFDataset): Lazy Dataset with columns ["x", "y"] specifying the tiles
             to be read.
     """
 
@@ -33,19 +33,19 @@ class OpenSlideTilesDataset(Dataset[NDArray[np.uint8]]):
         level: int | str,
         tile_extent_x: int | str,
         tile_extent_y: int | str,
-        tiles: pd.DataFrame,
+        tiles: HFDataset,
     ) -> None:
         """Initialize OpenSlideTilesDataset dataset.
 
         Args:
             slide_path: Path to the slide image.
             level: Level of the slide to read. If int, it is used as the level. If str,
-                it is used as the column name in the tiles DataFrame.
+                it is used as the column name in the tiles Dataset.
             tile_extent_x: Width of the tile. If int, it is used as the width. If str, it
-                is used as the column name in the tiles DataFrame.
+                is used as the column name in the tiles Dataset.
             tile_extent_y: Height of the tile. If int, it is used as the height. If str,
-                it is used as the column name in the tiles DataFrame.
-            tiles: DataFrame with columns ["x", "y"].
+                it is used as the column name in the tiles Dataset.
+            tiles: Dataset with columns ["x", "y"].
         """
         super().__init__()
         self.slide_path = Path(slide_path)
@@ -59,20 +59,14 @@ class OpenSlideTilesDataset(Dataset[NDArray[np.uint8]]):
 
     def __getitem__(self, idx: int) -> NDArray[np.uint8]:
         """Returns tile from the slide image at the specified index in RGB format."""
+        tile = self.tiles[idx]
+        level = self._get_from_tile(tile, self.level)
+        extent_x = self._get_from_tile(tile, self.tile_extent_x)
+        extent_y = self._get_from_tile(tile, self.tile_extent_y)
+
         with OpenSlide(self.slide_path) as slide:
-            tile = self.tiles.iloc[idx]
+            return slide.read_tile(tile["x"], tile["y"], extent_x, extent_y, level)
 
-            level = self._get_from_tile(tile, self.level)
-            extent_x = self._get_from_tile(tile, self.tile_extent_x)
-            extent_y = self._get_from_tile(tile, self.tile_extent_y)
-            x = int(tile["x"] * slide.level_downsamples[level])
-            y = int(tile["y"] * slide.level_downsamples[level])
-
-            rgba_region = slide.read_region((x, y), level, (extent_x, extent_y))
-            rgb_region = Image.alpha_composite(
-                Image.new("RGBA", rgba_region.size, (255, 255, 255)), rgba_region
-            ).convert("RGB")
-            return np.array(rgb_region)
-
-    def _get_from_tile(self, tile: pd.Series, key: int | str) -> int:
+    @staticmethod
+    def _get_from_tile(tile: dict[str, Any], key: int | str) -> int:
         return tile[key] if isinstance(key, str) else key
