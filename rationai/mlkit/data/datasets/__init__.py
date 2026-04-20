0123e12b96232dc06d7e44fd42b35c77b66386ef
Adam Kukučka <xkukuck1@fi.muni.cz>
2026-04-20 19:57:21 +0200

    refactor: move SlidesTilesLoader to its own module

--- rationai/mlkit/data/datasets/__init__.py
+++ rationai/mlkit/data/datasets/__init__.py
@@ -1,7 +1,6 @@
-from rationai.mlkit.data.datasets.meta_tiled_slides import (
-    MetaTiledSlides, SlidesTilesLoader
-)
+from rationai.mlkit.data.datasets.meta_tiled_slides import MetaTiledSlides
 from rationai.mlkit.data.datasets.openslide_tiles_dataset import OpenSlideTilesDataset
+from rationai.mlkit.data.datasets.slides_tiles_loader import SlidesTilesLoader
 
 
-__all__ = ["MetaTiledSlides", "SlidesTilesLoader", "OpenSlideTilesDataset"]
+__all__ = ["MetaTiledSlides", "OpenSlideTilesDataset", "SlidesTilesLoader"]
